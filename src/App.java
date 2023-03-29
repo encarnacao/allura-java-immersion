d874d6aeb038603a86133d2d671728bcfadb0a8e
encarnacao <cencarnacaoq@gmail.com>
2023-03-29 19:20:11 -0300

    feat: add styling

--- src/App.java
+++ src/App.java
@@ -24,12 +24,17 @@ public class App {
         JsonParser parser = new JsonParser();
         List<Map<String, String>> dataList = parser.parse(body);
         for (Map<String,String> movie: dataList){
-            System.out.println(movie.get("title"));
-            System.out.println(movie.get("imDbRating"));
+            System.out.println(new BoldText("Titulo: ")+movie.get("title"));
+            System.out.println(new BoldText("Poster: ")+movie.get("image"));
+
             int roundRating;
             try {
-                Float rating = Float.parseFloat(movie.get("imDbRating"));
+                String imdbRating = movie.get("imDbRating");
+                Float rating = Float.parseFloat(imdbRating);
                 roundRating = Math.round(rating);
+                String nota = "Nota: "+ imdbRating;
+                var ratingObj = new Rating(nota, roundRating);
+                System.out.println(ratingObj);
             } catch (Exception e){
                 System.out.println("imdb rating not available");
                 roundRating = 0;
